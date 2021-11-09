a3f523a39b88fce4405e481ef6c9931bbb181095
jh <kojh0111@naver.com>
2021-11-09 22:20:22 +0900

    Functions

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	const name string = "jh"
-	fmt.Println(name)
-	myname := "jh" // var myname string = "jh" 같은 의미!
-	myname = "koh jh"
-	fmt.Println(myname)
+	fmt.Println(multiply(2, 3))
+	totalLength, _ := lenAndUpper(("koh jh"))
+	fmt.Println(totalLength)
+	repeatMe("a", "b", "c", "d")
 }
