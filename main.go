4037abc6cf8b7112bfb5263b4d21a91d241af1e0
jh <kojh0111@naver.com>
2021-11-09 22:25:49 +0900

    Functions - Naked Return

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ func multiply(a, b int) int {
 	return a * b
 }
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func repeatMe(words ...string) {
@@ -19,7 +25,7 @@ func repeatMe(words ...string) {
 
 func main() {
 	fmt.Println(multiply(2, 3))
-	totalLength, _ := lenAndUpper(("koh jh"))
-	fmt.Println(totalLength)
+	totalLength, up := lenAndUpper(("koh jh"))
+	fmt.Println(totalLength, up)
 	repeatMe("a", "b", "c", "d")
 }
