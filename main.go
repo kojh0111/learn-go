b354029820cdb9f4a691db27b3805a48f690dbd9
jh <kojh0111@naver.com>
2021-11-09 22:32:15 +0900

    for, range

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done") // Function이 끝난 후에 실행되는 코드
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, up := lenAndUpper(("koh jh"))
-	fmt.Println(totalLength, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
