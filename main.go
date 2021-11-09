2b13222247f62c09415b33e8e7a502ab9aeb4e8d
jh <kojh0111@naver.com>
2021-11-09 22:28:17 +0900

    Functions - Defer

--- main.go
+++ main.go
@@ -5,27 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-// func lenAndUpper(name string) (int, string) {
-// 	return len(name), strings.ToUpper(name)
-// }
-
 func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done") // Function이 끝난 후에 실행되는 코드
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
 }
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
-
 func main() {
-	fmt.Println(multiply(2, 3))
 	totalLength, up := lenAndUpper(("koh jh"))
 	fmt.Println(totalLength, up)
-	repeatMe("a", "b", "c", "d")
 }
