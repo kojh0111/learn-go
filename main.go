808b0a0a275ac710c00b72ad3a6caefdbc17de1c
jh <kojh0111@naver.com>
2021-11-09 23:25:27 +0900

    Switch

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 )
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	koreanAge := age + 2
+	switch {
+	case koreanAge < 18:
+		return false
+	case koreanAge == 18:
+		return true
+	case koreanAge > 80:
 		return false
 	}
 	return true
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	nums := [6]int{14, 16, 18, 76, 78, 80}
+	for _, num := range nums {
+		fmt.Println(num, canIDrink(num))
+	}
 }
