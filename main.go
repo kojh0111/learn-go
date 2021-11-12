6d4c3ba506e1c4081a5f4f768bb9825bd22587fc
jh <kojh0111@naver.com>
2021-11-12 17:33:58 +0900

    Dictionary(Search)

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/kojh0111/learngo/accounts"
+	"github.com/kojh0111/learngo/dict"
 )
 
 func main() {
-	account := accounts.NewAccount("jh")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(5)
+	dictionary := dict.Dictionary{"first": "Foremost in position, rank, or importance"}
+	def, err := dictionary.Search("firts")
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(def)
 	}
-	fmt.Println(account)
+	def2, err2 := dictionary.Search("first")
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println(def2)
+	}
 }
