2910fefab90303236b59f5eaea2be85ca3076bed
jh <kojh0111@naver.com>
2021-11-13 13:33:33 +0900

    Dictionary(Delete)

--- main.go
+++ main.go
@@ -7,23 +7,15 @@ import (
 )
 
 func main() {
-	dictionary := dict.Dictionary{"first": "Foremost in position, rank, or importance"}
-	def, err := dictionary.Search("first")
+	dictionary := dict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(def)
-	}
-	newWord := "hello"
-	newMeaning := "Greeting"
-	err2 := dictionary.Add(newWord, newMeaning)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-	meaning, _ := dictionary.Search(newWord)
-	fmt.Println("found '", newWord, "' definition:", meaning)
-	err3 := dictionary.Add(newWord, newMeaning)
-	if err3 != nil {
-		fmt.Println(err3)
+		fmt.Println(word)
 	}
 }
