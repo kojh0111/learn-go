586e9881851f2222f7a3817082a03e1dbf169cdc
jh <kojh0111@naver.com>
2021-11-14 18:11:52 +0900

    go routines

--- main.go
+++ main.go
@@ -1,43 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.naver.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.amazonfsdafsa.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go helloCount("jh")
+	go helloCount("koh")
+	time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || (resp.StatusCode >= 400) {
-		return errRequestFailed
+func helloCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Hello", person, i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
