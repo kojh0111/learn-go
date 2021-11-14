37f0a7c8f2bb27ebb839ce01db2d84ad29a91799
jh <kojh0111@naver.com>
2021-11-14 17:59:57 +0900

    URL Checker

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.naver.com/",
 		"https://www.google.com/",
@@ -20,7 +21,15 @@ func main() {
 		"https://www.instagram.com/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -30,6 +39,5 @@ func hitURL(url string) error {
 	if err != nil || (resp.StatusCode >= 400) {
 		return errRequestFailed
 	}
-	fmt.Println("( StatusCode:", resp.StatusCode, ")")
 	return nil
 }
