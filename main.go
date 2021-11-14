52011c08ba26658e26b4b3fb9a21afe63fa6e7df
jh <kojh0111@naver.com>
2021-11-14 20:18:26 +0900

    URL Checker + go routines

--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan bool)
-	people := [2]string{"jh", "koh"}
-	for _, person := range people {
-		go helloCount(person, c)
-	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+type result struct {
+	url    string
+	status string
 }
 
-func helloCount(person string, c chan bool) {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second * 5)
-		fmt.Println("Hello ", person)
-		c <- true
+func main() {
+	c := make(chan result)
+	urls := []string{
+		"https://www.naver.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.amazonfsdafsa.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
 	}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || (resp.StatusCode >= 400) {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
 }
