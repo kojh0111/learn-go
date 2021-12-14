cd061df26ce369a7dba42a875af9d0e053f97859
jh <kojh0111@naver.com>
2021-12-14 20:44:50 +0900

    Extract Job

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	link     string
+	title    string
+	company  string
+	location string
+	salary   string
+	summary  string
+}
+
+// BaseURL - job search by pyhon in indeed
 var BaseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
@@ -17,11 +27,38 @@ func main() {
 	for i := 0; i <= totalPages; i++ {
 		getPage(i)
 	}
+	// getPage(0)
 }
 
 func getPage(page int) {
 	pageURL := BaseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkRes(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		link, _ := card.Attr("href")
+		title := card.Find("h2 > span").Text()
+		company := card.Find(".companyName").Text()
+		location := card.Find(".companyLocation").Text()
+		salary := card.Find(".salary-snippet > span").Text()
+		summary := card.Find(".job-snippet").Text()
+		job := extractedJob{
+			link:     link,
+			title:    title,
+			company:  company,
+			location: location,
+			salary:   salary,
+			summary:  summary}
+		fmt.Println(job.link)
+	})
 }
 
 func getPages() int {
